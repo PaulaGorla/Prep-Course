2a3eb9766cc9bd07b246062b88a0ffbbfc7e0b11
PaulaGorla <paulagorla19@gmail.com>
2022-12-12 11:03:35 -0300

    Homework 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -158,10 +158,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor === true) {
-    return "Soy verdadero"
-  } else {
+  if (valor === false) {
     return "Soy falso"
+  } else {
+    return "Soy verdadero"
   }
 }
 
